1df4867fc3f0d81133b3940e118f42ec5c3b2ed3
anggriawanmr <anggriawanmr@gmail.com>
2021-06-30 18:56:14 +0700

    Adding Seeds

--- app.js
+++ app.js
@@ -13,23 +13,39 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", ()=> {
     console.log("Database connected!");
-})
+});
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true}));
+
 app.get('/', (req,res) => {
     res.render('home');
-})
+});
 
-app.get('/makecampground', async (req,res) => {
-    const camp = new Campground({title: 'My Backyard', description: 'cheap camping!'});
-    await camp.save();
-    res.send(camp);
+app.get('/campgrounds', async (req,res) => {
+    const campgrounds = await Campground.find();
+    res.render('campgrounds/index', {campgrounds});
+});
+
+app.get('/campgrounds/new', (req,res) => {
+    res.render('campgrounds/new');
+});
+
+app.get('/campgrounds/:id', async(req,res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', {campground});
+});
+
+app.post('/campgrounds', async(req,res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 })
 
 app.listen(3000, ()=> {
     console.log('serving on port 3000');
-})
+});
