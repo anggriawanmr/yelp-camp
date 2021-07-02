7286c34a58747491651014bffec7a9e49e20ebdf
anggriawanmr <anggriawanmr@gmail.com>
2021-07-02 20:15:36 +0700

    Adding Partials

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,13 @@ const seedDB = async() => {
     await Campground.deleteMany({});
     for(i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla iure repudiandae magnam veniam modi, mollitia ipsum, aut neque unde itaque odio cum corrupti voluptates, earum expedita placeat alias suscipit totam! ',
+            price
         });
         await camp.save();
     }
