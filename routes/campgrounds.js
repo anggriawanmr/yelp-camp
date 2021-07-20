69e9a619992e3acbdc5eae0484c88099a2be4b1f
anggriawanmr <anggriawanmr@gmail.com>
2021-07-20 15:26:38 +0700

    Adding Image Upload

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,12 +2,20 @@ const express = require("express");
 const router = express.Router();
 const campgrounds = require("../controllers/campgrounds");
 const { isLoggedIn, validateCampground, isAuthor } = require("../middleware");
+var multer = require("multer");
+const { storage } = require("../cloudinary");
+const upload = multer({ storage });
 const catchAsync = require("../utils/catchAsync");
 
 router
   .route("/")
   .get(catchAsync(campgrounds.index))
-  .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+  .post(
+    isLoggedIn,
+    upload.array("image"),
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+  );
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
